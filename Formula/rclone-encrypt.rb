3e1fffc60bcc5b07118dcd59532a8876904155fc
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-06-23 21:33:14 +0000

    chore: update package manifests to v0.1.0

--- Formula/rclone-encrypt.rb
+++ Formula/rclone-encrypt.rb
@@ -6,20 +6,20 @@ class RcloneEncrypt < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yetanotherchris/rclone-encrypt-test-glm/releases/download/v0.1.0/rclone-encrypt-darwin-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "f9cb8264829ffa1d38d4db11f48df1416ed82ed9cda84f8a3aedf578641918f6"
     else
       url "https://github.com/yetanotherchris/rclone-encrypt-test-glm/releases/download/v0.1.0/rclone-encrypt-darwin-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "abc3d2fc73b352a3c10f6d6a521acd08a53f09ce95c6b5270dd1019d30115a24"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/yetanotherchris/rclone-encrypt-test-glm/releases/download/v0.1.0/rclone-encrypt-linux-arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "5aa4a65e39340a1eb83df341faca78f00dad36e8da906b61a7db814094597315"
     else
       url "https://github.com/yetanotherchris/rclone-encrypt-test-glm/releases/download/v0.1.0/rclone-encrypt-linux-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "db0da8ab60f36918799bf115d5fd446eb80416df658087b15ad02cb07e110be2"
     end
   end
 
